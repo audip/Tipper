c7e758926dcee32287e5581f3771fded4a4eb357
Aditya Purandare <aditya.p1993@hotmail.com>
2015-12-08 00:16:40 -0600

    Initial Commit

---
+++ tipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipper
+//
+//  Created by Aditya Purandare on 08/12/15.
+//  Copyright © 2015 Aditya Purandare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
