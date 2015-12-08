8a3d8f69654f1ada6629931cf56dbe450d883871
Aditya Purandare <aditya.p1993@hotmail.com>
2015-12-08 00:49:38 -0600

    Add basic design

--- tipper/ViewController.swift
+++ tipper/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
