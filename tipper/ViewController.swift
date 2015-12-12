7625302ff638911b61c2c4bdfd935f3a3c5ac90d
Aditya Purandare <aditya.p1993@hotmail.com>
2015-12-12 00:58:54 -0600

    Revert "Basic build ready"
    This reverts commit b9b767a0fe3a67cd92ab2ea7ea6e30272fdde1a9.

--- tipper/ViewController.swift
+++ tipper/ViewController.swift
@@ -9,8 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var tipControl: UISegmentedControl!
+
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipField: UITextField!
@@ -18,8 +17,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        tipLabel.text = "0.00"
-        totalLabel.text = "0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,21 +24,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func onEditingChange(sender: AnyObject) {
-        
-        var tipPercentages = [0.15, 0.18, 0.20, 0.25]
-        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
-        
-        let billAmount = NSString(string: tipField.text!).doubleValue
-        let tip = billAmount * tipPercentage
-        let totalAmount = billAmount + tip
-        
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", totalAmount)
-    }
 
-    @IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
-    }
 }
 
