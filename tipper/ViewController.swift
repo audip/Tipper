ac7f416d5ee66da61d3431c718ca9020b824b75c
Aditya Purandare <aditya.p1993@hotmail.com>
2015-12-13 23:59:05 -0600

    Fade in animations add

--- tipper/ViewController.swift
+++ tipper/ViewController.swift
@@ -16,8 +16,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var P2label: UILabel!
     @IBOutlet weak var P3label: UILabel!
     @IBOutlet weak var P4label: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var tipController: UISegmentedControl!
+    @IBOutlet weak var plusLabel: UILabel!
+    @IBOutlet weak var billLabel: UILabel!
+    @IBOutlet weak var IconP10: UIImageView!
+    @IBOutlet weak var IconP9: UIImageView!
+    @IBOutlet weak var IconP8: UIImageView!
+    @IBOutlet weak var IconP7: UIImageView!
+    @IBOutlet weak var IconP6: UIImageView!
+    @IBOutlet weak var IconP5: UIImageView!
+    @IBOutlet weak var IconP4: UIImageView!
+    @IBOutlet weak var IconP3: UIImageView!
+    @IBOutlet weak var IconP2: UIImageView!
+    @IBOutlet weak var IconP1: UIImageView!
     
     let defaults = NSUserDefaults.standardUserDefaults()
 
@@ -33,7 +44,24 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         //Animate from alpha
         self.tipLabel.alpha = 0
-        
+        self.billLabel.alpha = 0
+        self.plusLabel.alpha = 0
+        self.tipController.alpha = 0
+        self.P1label.alpha = 0
+        self.P2label.alpha = 0
+        self.P3label.alpha = 0
+        self.P4label.alpha = 0
+        self.IconP1.alpha = 0
+        self.IconP2.alpha = 0
+        self.IconP3.alpha = 0
+        self.IconP4.alpha = 0
+        self.IconP5.alpha = 0
+        self.IconP6.alpha = 0
+        self.IconP7.alpha = 0
+        self.IconP8.alpha = 0
+        self.IconP9.alpha = 0
+        self.IconP10.alpha = 0
+
         if(defaults.objectForKey("lastlogin") == nil)
         {
             defaults.setObject(NSDate(), forKey: "lastlogin")
@@ -106,20 +134,47 @@ class ViewController: UIViewController, UITextFieldDelegate {
         view.endEditing(true)
     }
     
-    /*@IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var tipField: UITextField!
-    @IBOutlet weak var P1label: UILabel!
-    @IBOutlet weak var P2label: UILabel!
-    @IBOutlet weak var P3label: UILabel!
-    @IBOutlet weak var P4label: UILabel!
-    @IBOutlet weak var tipController: UISegmentedControl!*/
     
     func textFieldDidBeginEditing(textField: UITextField) {
-        UIView.animateWithDuration(0.4, delay: 0.1, options: .CurveEaseInOut, animations: {
-            self.tipField.frame.origin = CGPointMake(self.view.frame.size.width/2, (self.view.frame.size.width/2) - 20 )
+        UIView.animateWithDuration(0.4, delay: 0.1, options: .CurveEaseOut, animations: {
+            self.tipField.frame.offsetInPlace(dx: 0, dy: -50)
+            self.billLabel.frame.offsetInPlace(dx: 0, dy: -50)
+            self.tipLabel.frame.offsetInPlace(dx: 0, dy: -50)
+            self.tipController.frame.offsetInPlace(dx: 0, dy: -50)
+            self.plusLabel.frame.offsetInPlace(dx: 0, dy: -50)
+            self.P1label.frame.offsetInPlace(dx: 0, dy: -50)
+            self.P2label.frame.offsetInPlace(dx: 0, dy: -50)
+            self.P3label.frame.offsetInPlace(dx: 0, dy: -50)
+            self.P4label.frame.offsetInPlace(dx: 0, dy: -50)
+            self.IconP1.frame.offsetInPlace(dx: 0, dy: -40)
+            self.IconP2.frame.offsetInPlace(dx: 0, dy: -40)
+            self.IconP3.frame.offsetInPlace(dx: 0, dy: -40)
+            self.IconP4.frame.offsetInPlace(dx: 0, dy: -40)
+            self.IconP5.frame.offsetInPlace(dx: 0, dy: -40)
+            self.IconP6.frame.offsetInPlace(dx: 0, dy: -40)
+            self.IconP7.frame.offsetInPlace(dx: 0, dy: -40)
+            self.IconP8.frame.offsetInPlace(dx: 0, dy: -40)
+            self.IconP9.frame.offsetInPlace(dx: 0, dy: -40)
+            self.IconP10.frame.offsetInPlace(dx: 0, dy: -40)
+            
             self.tipLabel.alpha = 1
-  
-
+            self.billLabel.alpha = 1
+            self.tipController.alpha = 1
+            self.plusLabel.alpha = 1
+            self.P1label.alpha = 1
+            self.P2label.alpha = 1
+            self.P3label.alpha = 1
+            self.P4label.alpha = 1
+            self.IconP1.alpha = 1
+            self.IconP2.alpha = 1
+            self.IconP3.alpha = 1
+            self.IconP4.alpha = 1
+            self.IconP5.alpha = 1
+            self.IconP6.alpha = 1
+            self.IconP7.alpha = 1
+            self.IconP8.alpha = 1
+            self.IconP9.alpha = 1
+            self.IconP10.alpha = 1
             
             },
             completion: {
